c5f19c7009e2eb0eceb5c336681ef0c627754f77
hatttruong <thuha0921@gmail.com>
2018-06-19 15:07:40 +0700

    add example of progress bar for apply function of pandas

--- src/progressbar.py
+++ src/progressbar.py
@@ -43,3 +43,22 @@ def simple_progressbar():
         cur_length = int(frac * max_length * 1. / 100)
         sys.stdout.write(pattern % ('=' * cur_length, frac))
         sys.stdout.flush()
+
+
+def pandas_progress_bar():
+    """
+    Reference:
+    https://stackoverflow.com/questions/18603270/progress-indicator-during-pandas-operations-python
+    """
+    import pandas as pd
+    import numpy as np
+    from tqdm import tqdm
+
+    df = pd.DataFrame(np.random.randint(0, int(1e8), (10000, 1000)))
+
+    # Create and register a new `tqdm` instance with `pandas`
+    # (can use tqdm_gui, optional kwargs, etc.)
+    tqdm.pandas()
+
+    # Now you can use `progress_apply` instead of `apply`
+    df.groupby(0).progress_apply(lambda x: x**2)
